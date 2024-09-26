ffe8fc91a40027e663d109b977782bfaaf1a7c47
pyrcl090 <70988666+pyrcl090@users.noreply.github.com>
2024-09-26 14:26:24 -0400

    cOq_c1_v1

---
+++ cOq.js
@@ -0,0 +1,9 @@
+let cOq = document.querySelector(".cOq");
+let about = document.querySelector(".about");
+let collections = document.querySelector(".collections");
+let workshops = document.querySelector(".workshops");
+let body = document.querySelector("body");
+
+// body.addEventListener("click", function(){
+//     body.classList.add(".coquille-1");   
+// })
